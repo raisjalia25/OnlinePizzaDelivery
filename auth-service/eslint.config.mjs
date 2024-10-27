493fe080ff83f4bf01abb2efadb56eab1d703705
raisjalia25 <raisjalia25@gmail.com>
2024-10-27 23:13:50 +0530

    test husky 1

--- auth-service/eslint.config.mjs
+++ auth-service/eslint.config.mjs
@@ -7,6 +7,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
+    ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
   },
   {
     languageOptions: {
