f0ee3fa6e4a8891551b09890380fa9da856b2a0a
raisjalia25 <raisjalia25@gmail.com>
2024-10-27 23:46:15 +0530

    add config

--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -1,9 +1,3 @@
-function welcome(name:string) {
-    console.log('hello');
-    const user ={
-        name:'Rais'
-    }
-    const fname = user['name']
-    return name+fname
-}
-welcome("Rais")
+import { Config } from "./config";
+
+console.log(Config.PORT);
