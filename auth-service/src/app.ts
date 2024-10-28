40cd6d506daa469bc8a10e11e570986b20ab21a0
raisjalia25 <raisjalia25@gmail.com>
2024-10-28 16:14:58 +0530

    fix package.json spacing

---
+++ auth-service/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send(`Welcome to Auth Service`)
+})
+
+
+export default app
--- auth-service/src/server.ts
+++ auth-service/src/server.ts
@@ -1,3 +1,14 @@
 import { Config } from "./config";
-
-console.log(Config.PORT);
+import app from "./app"
+const startServer = ()=> {
+    const PORT= Config.PORT
+    try{
+        app.listen(PORT,()=>console.log(`Listening on PORT ${PORT}`));
+    
+    }    
+    catch(err){
+        console.error(err)
+        process.exit(1)
+    }
+}
+startServer()
